3daa1e3d6944ca644dc5e135b0a8ebf1b55d98fa
Awais <amusmani@gmail.com>
2022-03-30 10:07:38 +0500

    designing

--- shop/src/components/product/product-question.tsx
+++ shop/src/components/product/product-question.tsx
@@ -8,17 +8,32 @@ function ProductQuestion() {
 		  <a href="#" className="text-blue text-2xl underline pl-2 pr-2">Register</a>to ask questions to seller</p>
 		  <p>Other questions answered by one dollershop (Rawalpindi) (1)</p>
 
-		  <p className="">
-		  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+		    <div className="mb-5">
+			<p className="text-blue font-semibold mb-1">
+		  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
 			<path stroke-linecap="round" stroke-linejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
 			</svg>
 			  Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+			<p className="">
+			<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+			<path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+			</svg>
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+			</div>
 
-			  <p className="">
-			  <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
-				<path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
-				</svg>
+			
+		    <div className="mb-5">
+			<p className="text-blue font-semibold mb-1">
+		  <svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+			<path stroke-linecap="round" stroke-linejoin="round" d="M8.228 9c.549-1.165 2.03-2 3.772-2 2.21 0 4 1.343 4 3 0 1.4-1.278 2.575-3.006 2.907-.542.104-.994.54-.994 1.093m0 3h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" />
+			</svg>
 			  Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+			<p className="">
+			<svg xmlns="http://www.w3.org/2000/svg" className="h-6 w-6 inline-block" fill="none" viewBox="0 0 24 24" stroke="currentColor" stroke-width="2">
+			<path stroke-linecap="round" stroke-linejoin="round" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+			</svg>
+			Lorem Ipsum is simply dummy text of the printing and typesetting industry.</p>
+			</div>
 	  </div>
 	</div>
   )
