54017a1f2de98f6ffb11dca17b64c7ca215a72a4
muzammalrahim <creative.joomdev@gmail.com>
2022-04-06 14:35:57 +0500

    category-css

--- shop/src/components/ui/button.tsx
+++ shop/src/components/ui/button.tsx
@@ -31,11 +31,11 @@ const Button = forwardRef<HTMLButtonElement, ButtonProps>((props, ref) => {
   const rootClassName = cn(
     "text-[13px] md:text-sm leading-4 inline-flex items-center cursor-pointer transition ease-in-out duration-300 font-semibold font-body text-center justify-center border-0 border-transparent placeholder-white focus-visible:outline-none focus:outline-none focus:bg-opacity-80",
     {
-      "bg-heading text-white px-5 md:px-6 lg:px-8 py-4 md:py-3.5 lg:py-4 hover:text-white hover:bg-gray-600 hover:shadow-cart":
+      "bg-bg-blue text-white px-5 md:px-6 lg:px-8 py-4 md:py-3.5 lg:py-4 hover:text-white hover:bg-gray-600 hover:shadow-cart":
         variant === "flat",
-      "h-11 md:h-12 px-5 bg-heading text-white py-2 transform-none normal-case hover:text-white hover:bg-gray-600 hover:shadow-cart":
+      "h-11 md:h-12 px-5 bg-blue text-white py-2 transform-none normal-case hover:text-white hover:bg-gray-600 hover:shadow-cart":
         variant === "slim",
-      "h-auto px-3.5 md:px-4 bg-heading text-white py-2 transform-none normal-case hover:text-white hover:bg-gray-600 hover:shadow-cart":
+      "h-auto px-3.5 md:px-4 bg-blue text-white py-2 transform-none normal-case hover:text-white hover:bg-gray-600 hover:shadow-cart":
         variant === "slimSmall",
       "h-11 md:h-12 px-5 bg-gray-200 text-heading py-2 transform-none normal-case hover:bg-gray-300 md:leading-4":
         variant === "smoke",
