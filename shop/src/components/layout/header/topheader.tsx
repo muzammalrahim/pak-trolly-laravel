2f7ff2732d757bf17e13fa9cc0e418ec56e3d582
muzammalrahim <creative.joomdev@gmail.com>
2022-04-08 11:29:11 +0500

    designing

--- shop/src/components/layout/header/topheader.tsx
+++ shop/src/components/layout/header/topheader.tsx
@@ -9,10 +9,10 @@ export default function TopHeader() {
 
 		<div className="top-header  grid grid-cols-12 pt-5 pb-5">
 			
-			<div className="lg:col-span-6 justify-center  md:col-span-12 sm:col-span-12 col-span-12 sm:text-center sm:pb-0">
-				<p className="text-sm text-white text-left p-0 m-0">Tell a friends about Electshop Electronics & get 30% off your next order.</p>
+			<div className="lg:col-span-6 xl:col-span-6 2xl:col-span-6  justify-center md:col-span-6  sm:col-span-12  md:text-center sm:w-full  sm:text-center sm:pb-0 sm:text-xs">
+				<p className="text-sm text-white sm:text-center text-left p-0 m-0">Tell a friends about Electshop Electronics & get 30% off your next order.</p>
 			</div>
-			<div className="lg:col-span-6 justify-center  md:col-span-12 sm:col-span-12 col-span-12 lg:text-right sm:pb-0">
+			<div className="lg:col-span-6 xl:col-span-6 2xl:col-span-6 justify-center md:col-span-6 sm:mt-2 sm:col-span-12 sm:w-full lg:text-right sm:pb-0 sm:text-center sm:text-xs">
 				<ul>
 					<li className="inline-block text-white justify-center text-right">
 						<a href="#"  className="text-white text-xs pr-5 mr-5 border-r-1 border-white">Need Help?</a>
