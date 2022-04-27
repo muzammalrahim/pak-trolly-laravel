5a5f103345b2c95538a8993fe3b2bddfb720c037
muzammalrahim <creative.joomdev@gmail.com>
2022-04-27 17:06:23 +0500

    two way verification

--- shop/src/components/auth/otp/two-factor.tsx
+++ shop/src/components/auth/otp/two-factor.tsx
@@ -9,6 +9,9 @@ import { AUTH_TOKEN } from "@lib/constants";
 import { useAtom } from "jotai";
 import { authorizationAtom } from "@store/authorization-atom";
 import { OTPLoginForm } from "@components/auth/otp/otp-login-form";
+import Label from "@components/ui/label";
+import Button from "@components/ui/button";
+import MobileOtpInput from "react-otp-input";
 
 type Props = {
   layout?: "modal" | "page";
@@ -50,12 +53,36 @@ const TwoFactorLogin: React.FC<Props> = ({ layout = "modal" }) => {
           <Logo />
         </div>
         <p className="text-sm md:text-base text-body mt-3 sm:mt-4 mb-8 sm:mb-10">
-          Enter your 5-Digit Code.
+          {/* Enter your 5-Digit Code. */}
+          We'll send you a link to reset your password
         </p>
       </div>
 
-      <OTPLoginForm onLoginSuccess={onLoginSuccess} />
+      {/* <OTPLoginForm onLoginSuccess={onLoginSuccess} /> */}
 
+      <div>
+        <Label>{t("text-otp-code")}</Label>
+        <MobileOtpInput
+          // value={value}
+          // onChange={onChange}
+          numInputs={6}
+          separator={<span className="hidden sm:inline-block sm:mx-2">-</span>}
+          containerStyle="justify-center space-x-2 sm:space-x-0 mb-5 md:mb-0"
+          inputStyle="flex items-center justify-center !w-full sm:!w-11 appearance-none transition duration-300 ease-in-out text-heading text-sm focus:outline-none focus:ring-0 border border-gray-100 rounded focus:border-heading h-12"
+          disabledStyle="!bg-gray-100"
+        />
+      </div>
+      <br />
+      <div className="relative">
+        <Button
+          type="submit"
+          // loading={otpLoginLoading}
+          // disabled={otpLoginLoading}
+          className="h-11 md:h-12 w-full mt-1.5"
+        >
+          {t("common:text-login")}
+        </Button>
+      </div>
       <div className="flex flex-col items-center justify-center relative text-sm text-heading mt-8 sm:mt-10 mb-6 sm:mb-7">
         <hr className="w-full border-gray-300" />
         <span className="absolute -top-2.5 px-2 bg-white">
@@ -77,7 +104,3 @@ const TwoFactorLogin: React.FC<Props> = ({ layout = "modal" }) => {
 };
 
 export default TwoFactorLogin;
-
-
-
-
